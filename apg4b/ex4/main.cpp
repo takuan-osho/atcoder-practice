03a026e7751bea3528e46b552f7bb605b697edf8
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-07 16:14:09 +0900

    Add APG4b_cs file

---
+++ apg4b/ex4/main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+
+  // 以下のコメント/* */を消して追記する
+  cout << seconds * 1 << endl;
+  cout << seconds * 2 << endl;
+  cout << seconds * 5 << endl;
+  cout << seconds * 10 << endl;
+}
