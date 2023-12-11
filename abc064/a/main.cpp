6b5189d8b1a2b56e9ee8aa67d68c152edf1da620
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 13:19:50 +0900

    Add abc064_a file

---
+++ abc064/a/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int r, g, b;
+  cin >> r >> g >> b;
+  int color_num = r * 100 + g * 10 + b;
+  if (color_num % 4 == 0) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
