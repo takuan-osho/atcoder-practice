2cc511af7e4969433988269230b018b61b524f6b
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 13:37:05 +0900

    Add abc128_a file

---
+++ abc128/a/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, P;
+  cin >> A >> P;
+  int pieces = A * 3 + P;
+  int pies = pieces / 2;
+  cout << pies << endl;
+  return 0;
+}
