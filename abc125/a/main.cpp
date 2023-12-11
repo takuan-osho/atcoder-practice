e865d60c22c30a855da16206d02875d895f36174
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 15:11:19 +0900

    Add abc125_a file

---
+++ abc125/a/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B, T;
+  cin >> A >> B >> T;
+  int biscuit_num = (T / A) * B;
+  cout << biscuit_num << endl;
+  return 0;
+}
