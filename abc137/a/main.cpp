8bfc392d1766caf86ffde5745a81e375bf306413
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 10:42:46 +0900

    Add abc137_a file

---
+++ abc137/a/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  int answer = max({A + B, A - B, A * B});
+  cout << answer << endl;
+}
