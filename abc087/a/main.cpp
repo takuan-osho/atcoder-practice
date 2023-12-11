e42a0eec52766d92380040b1462808b89b0a5949
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 13:11:46 +0900

    Add abc087_a file

---
+++ abc087/a/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int X, A, B;
+  cin >> X >> A >> B;
+
+  int remained_price = (X - A) % B;
+  cout << remained_price << endl;
+  return 0;
+}
