8103b7fe816d551552ce713951d4303d90f62bba
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-11 12:07:12 +0900

    Add abc127_a file

---
+++ abc127/a/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  int price;
+
+  if (A <= 5) {
+    price = 0;
+  } else if (A >= 6 and A <= 12) {
+    price = B / 2;
+  } else {
+    price = B;
+  }
+  cout << price << endl;
+  return 0;
+}
