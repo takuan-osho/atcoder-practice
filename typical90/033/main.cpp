ec632d039cf45f91ed0f7d920a57c4894855409d
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-12 09:29:53 +0900

    Add typical90_ag file

---
+++ typical90/033/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int H, W;
+  cin >> H >> W;
+
+  if (H == 1 || W == 1) {
+    cout << H * W << endl;
+  } else {
+    cout << ((H + 1) / 2) * ((W + 1) / 2) << endl;
+  }
+  return 0;
+}
