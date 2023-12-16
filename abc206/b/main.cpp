009f74ea2180b625aad4aae927d359a513daab2d
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-16 18:04:40 +0900

    Add abc206_b file

---
+++ abc206/b/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  int sum_money = 0;
+  int day = 0;
+
+  while (true) {
+    day += 1;
+    sum_money += day;
+    if (sum_money >= N) {
+      break;
+    }
+  }
+  cout << day << endl;
+  return 0;
+}
