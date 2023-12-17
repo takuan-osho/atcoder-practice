e17bc657dabbba5b2939df2d90f1b3ca3c7e1523
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-17 14:18:21 +0900

    Add abc164_b file

---
+++ abc164/b/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B, C, D;
+  cin >> A >> B >> C >> D;
+
+  while (A > 0) {
+    C -= B;
+    if (C <= 0) {
+      cout << "Yes" << endl;
+      break;
+    }
+    A -= D;
+    if (A <= 0) {
+      cout << "No" << endl;
+      break;
+    }
+  }
+
+  return 0;
+}
