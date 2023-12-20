79e51f7e95cc2006828c4c823367aa9e13e10ed1
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-20 23:04:58 +0900

    Add abc158_c file

---
+++ abc158/c/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  for (int i = 1; i < 10000; i++) {
+    int a = i * 8 / 100;
+    int b = i * 10 / 100;
+    if (a == A && b == B) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+  return 0;
+}
