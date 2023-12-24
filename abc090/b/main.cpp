a32475236c0b2db17ead24d9d872c4fe681721e2
SHIMIZU Taku <shimizu.taku@gmail.com>
2023-12-24 15:15:39 +0900

    Add abc090_b file

---
+++ abc090/b/main.cpp
@@ -0,0 +1,36 @@
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>    // bitset
+#include <cctype>    // isupper, islower, isdigit, toupper, tolower
+#include <cstdint>   // int64_t, int*_t
+#include <cstdio>    // printf
+#include <deque>     // deque
+#include <iostream>  // cout, endl, cin
+#include <map>       // map
+#include <queue>     // queue, priority_queue
+#include <set>       // set
+#include <stack>     // stack
+#include <string>    // string, to_string, stoi
+#include <tuple>     // tuple, make_tuple
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <utility>       // pair, make_pair
+#include <vector>        // vector
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  string s, reversed;
+  int kaibun_sum = 0;
+  for (int i = A; i <= B; i++) {
+    auto s = to_string(i);
+    auto reversed = to_string(i);
+    reverse(reversed.begin(), reversed.end());
+
+    if (s == reversed) {
+      kaibun_sum++;
+    }
+  }
+  cout << kaibun_sum << endl;
+}
