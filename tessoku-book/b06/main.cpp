65cd1a2c7b029604d299e7ad8f8b032cadf7673a
SHIMIZU Taku <shimizu.taku@gmail.com>
2024-01-19 10:06:06 +0900

    WIP

---
+++ tessoku-book/b06/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin A[i];
+  }
+
+  int Q;
+  cin >> Q;
+  vector<int> L(Q), R(Q);
+  for (int i = 0; i < Q; i++) {
+    cin >> L[i] >> R[i];
+  }
+}
